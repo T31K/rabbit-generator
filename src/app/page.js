b8390b48528d16ac25846160d6ada901c43c1e32
T31K <t31kmunwong@gmail.com>
2024-01-12 17:16:09 +0800

    move footer to avoid hydration issues

--- src/app/page.js
+++ src/app/page.js
@@ -20,74 +20,83 @@ export default function Home() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, accept: 'image/*' });
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
-      <div className="w-full max-w-md mx-auto flex items-center justify-center flex-col">
-        <h1 className="text-center tracking-tight mb-4">Make your own</h1>
-        <Image
-          src="/logo.png"
-          width={200}
-          height={500}
-          alt="Picture of the author"
-        />
-      </div>
-      <div
-        className="relative mt-5"
-        ref={ref}
-      >
-        <Image
-          src="/rabbit.png"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
-        <div
-          {...getRootProps()}
-          className="bg-black h-[461px] w-[347px] left-[15px] absolute top-[15px] rounded-[30px] flex items-center justify-center overflow-hidden"
-          style={{ transform: 'scale(var(--scale-factor, 1))', transformOrigin: 'top left' }}
-        >
-          <input {...getInputProps()} />
-          {droppedImage ? (
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              src={droppedImage}
-              alt="Dropped"
-              style={{ width: '100%', height: '100%' }}
-            />
-          ) : isDragActive ? (
-            <div className="flex flex-col items-center">
-              <ArrowBigDownDash
-                className="text-white"
-                size={72}
-              />
-              <p className="text-white tracking-tight font-medium">Drop your image here</p>
-            </div>
-          ) : (
-            <div className="flex flex-col items-center">
-              <ArrowBigDownDash
-                className="text-white"
-                size={72}
-              />
-              <p className="text-white tracking-tight font-medium">Drop your image here</p>
-            </div>
-          )}
+    <>
+      <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+        <div className="w-full max-w-md mx-auto flex items-center justify-center flex-col">
+          <h1 className="text-center tracking-tight mb-4">Make your own</h1>
+          <Image
+            src="/logo.png"
+            width={200}
+            height={500}
+            alt="Picture of the author"
+          />
         </div>
-      </div>
-      <Button
-        onClick={getPng}
-        className="mt-4"
-      >
-        Download SVG
-      </Button>
-      <style
-        jsx
-        global
-      >{`
-        @media (max-width: 768px) {
-          :root {
-            --scale-factor: 0.7; // Adjust this value to scale the black box on smaller screens
+        <div
+          className="relative mt-5"
+          ref={ref}
+        >
+          <Image
+            src="/rabbit.png"
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+          <div
+            {...getRootProps()}
+            className="bg-black h-[461px] w-[347px] left-[15px] absolute top-[15px] rounded-[30px] flex items-center justify-center overflow-hidden"
+            style={{ transform: 'scale(var(--scale-factor, 1))', transformOrigin: 'top left' }}
+          >
+            <input {...getInputProps()} />
+            {droppedImage ? (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={droppedImage}
+                alt="Dropped"
+                style={{ width: '100%', height: '100%' }}
+              />
+            ) : isDragActive ? (
+              <div className="flex flex-col items-center">
+                <ArrowBigDownDash
+                  className="text-white"
+                  size={72}
+                />
+                <p className="text-white tracking-tight font-medium">Drop your image here</p>
+              </div>
+            ) : (
+              <div className="flex flex-col items-center">
+                <ArrowBigDownDash
+                  className="text-white"
+                  size={72}
+                />
+                <p className="text-white tracking-tight font-medium">Drop your image here</p>
+              </div>
+            )}
+          </div>
+        </div>
+        <Button
+          onClick={getPng}
+          className="mt-4"
+        >
+          Download SVG
+        </Button>
+        <style
+          jsx
+          global
+        >{`
+          @media (max-width: 768px) {
+            :root {
+              --scale-factor: 0.7; // Adjust this value to scale the black box on smaller screens
+            }
           }
-        }
-      `}</style>
-    </main>
+        `}</style>
+      </main>
+      <footer className=" bg-gray-100 mx-auto h-[10vh] flex items-center justify-center">
+        <div className="mx-auto w-full flex items-center justify-center">
+          <p className="text-sm text-gray-500 sm:text-center dark:text-gray-400 tracking-tight font-semibold">
+            Made with ❤️‎ ‎‎ by T31K
+          </p>
+        </div>
+      </footer>
+    </>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className=" bg-gray-100 mx-auto h-[10vh] flex items-center justify-center">
       <div className="mx-auto w-full flex items-center justify-center">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400 tracking-tight font-semibold">Made with ❤️‎ ‎‎ by T31K</span>
+          <p className="text-sm text-gray-500 sm:text-center dark:text-gray-400 tracking-tight font-semibold">Made with ❤️‎ ‎‎ by T31K</p>
       </div>
     </footer>
   );
